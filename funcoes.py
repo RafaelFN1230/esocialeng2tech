f6fa293b660a4164a041b1789b0069c3469a4e15
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-04-06 19:09:52 -0300

    Fix: Fixes bugs related to the order of the coloumns

--- funcoes.py
+++ funcoes.py
@@ -6,15 +6,31 @@ import pandas as pd
 def extrair_tabela(filePDF, pagIni, pagFin):  
     pdf = filePDF
     years = [] 
-    dataset = {}
     excel_path = "planilha-ficha_financeira ("+pagIni+"-"+pagFin+").xlsx"
     current_year = None
+
+    dataset = {
+        'periodo':{
+            'elementos': {}
+            }, 
+        'proventos':{
+            'elementos': {}
+            }, 
+        'descontos':{
+            'elementos': {}
+            }, 
+        'totais':{
+            'elementos': {}
+            }
+        }
+
 
     for pag in range(int(pagIni)-1, int(pagFin)):
         pagina = pdf.pages[pag]
         tabelas = pagina.extract_tables()
         tabela = tabelas[0]
         unwanted_columns = []
+        dataset['descontos']['status'] = False
         for index, coluna in enumerate(tabela):
             if coluna[0].lower() in ['dados bancários', 'banco', 'agência', 'conta']:
                 unwanted_columns.append(index)
@@ -30,12 +46,15 @@ def extrair_tabela(filePDF, pagIni, pagFin):
                     current_year = int(periodo[1])
                     years.append(current_year)
                 unwanted_columns.append(index)
+        
         dados = _dataset_cleaner(tabela, unwanted_columns)
         for dado in dados:
+            if dado[0] == 'DESCONTOS':
+                dataset['descontos']['status'] = True
             _add_element(dataset, dado[0], dado, years[0], current_year)
 
-    final_daset = _create_list(dataset)
-    _generate_excel(final_daset['chaves'], final_daset['elementos'], excel_path)
+    final_dataset = _create_list(dataset)
+    _generate_excel(final_dataset['chaves'], final_dataset['elementos'], excel_path)
 
 def _dataset_cleaner(dataset:list, unwanted_columns:list):
     unwanted_columns.reverse()
@@ -56,25 +75,24 @@ def _generate_excel(chaves, elementos, excel_path):
     df.to_excel(excel_path, sheet_name="eSocial", index=False, header=False)
 
 
-def _add_element(dicionario, chave, elementos:list, first_year, current_year):
-    if chave in dicionario:
-        for elemento in elementos:
-            if elemento != chave:
-                dicionario[chave].append(elemento)
+def _add_element(dataset, chave, elementos:list, first_year, current_year):
+    if chave in ['TOTAL DE PROVENTOS', 'TOTAL DE DESCONTOS', 'TOTAL LÍQUIDO A RECEBER']:
+        _separa_elementos(dataset, chave, elementos,first_year, current_year, destino = 'totais')
+    elif dataset['descontos']['status']:
+        _separa_elementos(dataset, chave, elementos,first_year, current_year, destino = 'descontos')
+    elif chave == 'PERÍODO':
+        _separa_elementos(dataset, chave, elementos,first_year, current_year, destino = 'periodo')
     else:
-        elementos = _fix_timeline(elementos, first_year, current_year)
-        dicionario[chave] = elementos
+        _separa_elementos(dataset, chave, elementos, first_year, current_year, destino = 'proventos')
 
 
 def _create_list(dictionary:dict):
-    chaves = []
-    elementos = []
-
-    for chave, valores in dictionary.items():
-        chaves.append(chave)
-        elementos.append(valores)
-    list = {'chaves':chaves, 'elementos':elementos}
-    return list
+    lista = {'chaves': [], 'elementos': []}
+    _junta_elementos(dictionary['periodo']['elementos'], lista)
+    _junta_elementos(dictionary['proventos']['elementos'], lista)
+    _junta_elementos(dictionary['descontos']['elementos'], lista)
+    _junta_elementos(dictionary['totais']['elementos'], lista)
+    return lista
 
 def _fix_timeline(elementos:list, first_year:int, current_year:int):
     difference_in_years = current_year - first_year
@@ -83,3 +101,18 @@ def _fix_timeline(elementos:list, first_year:int, current_year:int):
         for _ in range(n_linhas):
             elementos.insert(1, '')
     return elementos
+
+def _separa_elementos (dataset, chave, elementos, first_year, current_year, destino):
+    if chave in dataset[destino]['elementos']:
+            for elemento in elementos:
+                if elemento != chave:
+                    dataset[destino]['elementos'][chave].append(elemento)
+    else:
+        elementos = _fix_timeline(elementos, first_year, current_year)
+        dataset[destino]['elementos'][chave] = elementos
+
+def _junta_elementos(dictionary:dict, lista:dict):
+    for chave, valores in dictionary.items():
+        lista['chaves'].append(chave)
+        lista['elementos'].append(valores)        
+    return lista
