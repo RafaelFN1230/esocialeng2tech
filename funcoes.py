c715e65510cef6532d20716fcaa54e539d4e2768
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-04-06 11:43:24 -0300

    Feat: Cleans dataset, removing unwanted lines, and separates the data in dates and the rest of the data

--- funcoes.py
+++ funcoes.py
@@ -5,19 +5,46 @@ import pdfplumber
 import pandas as pd
 
 def extrair_tabela(filePDF, pagIni, pagFin):  
-  # Abrir o arquivo PDF
-  pdf = filePDF
- 
-  for pag in range(int(pagIni)-1, int(pagFin)): # paginaInicial, paginaFinal
-      pagina = pdf.pages[pag]
-      tabela = pagina.extract_tables()
+    # Abrir o arquivo PDF
+    pdf = filePDF
+    date = [] 
+    dataset = []
+    for pag in range(int(pagIni)-1, int(pagFin)): # paginaInicial, paginaFinal
+        pagina = pdf.pages[pag]
+        tabelas = pagina.extract_tables()
+        tabela = tabelas[0]
+        unwanted_columns = []
+        # Salvar a primeira tabela como um arquivo XLS - len(tabela)
+        df1 = pd.DataFrame()
+        for index, coluna in enumerate(tabela):
+            if coluna[0].lower() in ['dados bancários', 'banco', 'agência', 'conta']:
+                unwanted_columns.append(index)
+            elif coluna.count('') == len(coluna):
+                unwanted_columns.append(index)
+            elif coluna.count(None) == len(coluna)-1:
+                pass
+            elif 'JAN/' in coluna[1]:
+                for value in coluna:
+                    date.append(value)
+                unwanted_columns.append(index)
+            #print('coluna: ',coluna)
+        tabela = _dataset_cleaner(tabela, unwanted_columns)
+        for clean_colunm in tabela:
+            dataset.append(clean_colunm)
 
-      # Salvar a primeira tabela como um arquivo XLS - len(tabela)
-      df1 = pd.DataFrame()
-      for qtd in range(len(tabela)):
-          df1 = pd.DataFrame(data=tabela[qtd], dtype=object)
+        print('dataset: ', dataset)
+            
+        for qtd in range(len(tabela)):
+            df1 = pd.DataFrame(data=tabela[qtd], dtype=object)
 
-          df1Populado = df1.values
+            df1Populado = df1.values
 
-          if (df1Populado[0][1] != ""):
-            df1.to_excel("planilha-pag_"+str(pag+1)+"_tab_"+str(qtd)+".xlsx", sheet_name="eSocial", header=False, index=False)
+            if (df1Populado[0][1] != ""):
+                df1.to_excel("planilha-pag_"+str(pag+1)+"_tab_"+str(qtd)+".xlsx", sheet_name="eSocial", header=False, index=False)
+
+def _dataset_cleaner(dataset, unwanted_columns):
+    unwanted_columns.reverse()
+    for index in unwanted_columns:
+      dataset.pop(index)
+    return dataset
+
