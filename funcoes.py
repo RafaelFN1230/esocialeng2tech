9fdb3d99e53dc17596e7e3328b9f6afa854fbeb1
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-04-06 15:00:30 -0300

    Feat: Adds the function to create a dynamic dictionary to store the values of the dataset, where the key will be used to index the values. These values will be send to _generate_excel function, where it will transpose the array and create the excel file.

--- funcoes.py
+++ funcoes.py
@@ -1,21 +1,19 @@
 #pip install pdfplumber
 #python.exe -m pip install --upgrade pip
 
-import pdfplumber
 import pandas as pd
 
 def extrair_tabela(filePDF, pagIni, pagFin):  
-    # Abrir o arquivo PDF
     pdf = filePDF
     date = [] 
-    dataset = []
-    for pag in range(int(pagIni)-1, int(pagFin)): # paginaInicial, paginaFinal
+    dataset = {}
+    excel_path = "planilha-ficha_financeira ("+pagIni+"-"+pagFin+").xlsx"
+
+    for pag in range(int(pagIni)-1, int(pagFin)):
         pagina = pdf.pages[pag]
         tabelas = pagina.extract_tables()
         tabela = tabelas[0]
         unwanted_columns = []
-        # Salvar a primeira tabela como um arquivo XLS - len(tabela)
-        df1 = pd.DataFrame()
         for index, coluna in enumerate(tabela):
             if coluna[0].lower() in ['dados bancários', 'banco', 'agência', 'conta']:
                 unwanted_columns.append(index)
@@ -24,27 +22,45 @@ def extrair_tabela(filePDF, pagIni, pagFin):
             elif coluna.count(None) == len(coluna)-1:
                 pass
             elif 'JAN/' in coluna[1]:
-                for value in coluna:
-                    date.append(value)
+                if coluna[1] not in date:
+                    coluna[0] = 'PERÍODO'
+                    _add_element(dataset, coluna[0], coluna)
+                    for celula in coluna:
+                        date.append(celula)
                 unwanted_columns.append(index)
-            #print('coluna: ',coluna)
-        tabela = _dataset_cleaner(tabela, unwanted_columns)
-        for clean_colunm in tabela:
-            dataset.append(clean_colunm)
+        dados = _dataset_cleaner(tabela, unwanted_columns)
+        for dado in dados:
+            _add_element(dataset, dado[0], dado)
 
-        print('dataset: ', dataset)
-            
-        for qtd in range(len(tabela)):
-            df1 = pd.DataFrame(data=tabela[qtd], dtype=object)
+    final_daset = _create_list(dataset)
+    _generate_excel(final_daset['chaves'], final_daset['elementos'], excel_path)
 
-            df1Populado = df1.values
-
-            if (df1Populado[0][1] != ""):
-                df1.to_excel("planilha-pag_"+str(pag+1)+"_tab_"+str(qtd)+".xlsx", sheet_name="eSocial", header=False, index=False)
-
-def _dataset_cleaner(dataset, unwanted_columns):
+def _dataset_cleaner(dataset:list, unwanted_columns:list):
     unwanted_columns.reverse()
     for index in unwanted_columns:
-      dataset.pop(index)
+        dataset.pop(index)
     return dataset
 
+def _generate_excel(chaves, elementos, excel_path):
+    df = pd.DataFrame(elementos, index=chaves).T
+    df.to_excel(excel_path, sheet_name="eSocial", index=False, header=False)
+
+
+def _add_element(dicionario, chave, elementos:list):
+    if chave in dicionario:
+        for elemento in elementos:
+            if elemento != chave:
+                dicionario[chave].append(elemento)
+    else:
+        dicionario[chave] = elementos
+
+
+def _create_list(dictionary:dict):
+    chaves = []
+    elementos = []
+
+    for chave, valores in dictionary.items():
+        chaves.append(chave)
+        elementos.append(valores)
+    list = {'chaves':chaves, 'elementos':elementos}
+    return list
